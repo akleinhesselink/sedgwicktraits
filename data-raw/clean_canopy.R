7e774c6050255d905a56b46224c8cafa7865f156
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-09-19 16:36:05 -0700

    Add script to calculate canopy traits
    Complicated analysis that brings in data for biomass, SLA and leaf
    area data.
    
    Calculate total leaf area per plant in one of two ways; if scanned
    then the leaf area of the three sample leaves is added to the leaf
    area of the extra nonsample leaves; if remaining leaves were not
    scanned than leaf area was calculated by taking the total leaf
    weight and multiplying by the avg sla for that species.
    
    currently only uses leaves without petioles, can add a new
    script for the species that only have petiole data.

--- data-raw/clean_canopy.R
+++ data-raw/clean_canopy.R
@@ -7,19 +7,36 @@ outfile <- 'data-raw/clean_canopy.R'
 alias <- read_csv('data-raw/alias.csv')
 canopy <- read_csv('data-raw/canopy_dimensions.csv')
 agb <- read_csv('data-raw/aboveground_biomass_weights.csv')
+leaf_traits <- read_csv('data-raw/leaf_traits.csv')
 
 leaf_area <- read_csv('data-raw/leaf_area.csv')
 
-leaf_area <- 
+avg_SLA <- 
+  leaf_traits %>% 
+  filter( plot == 'non_plot') %>% 
+  filter( !censor) %>% 
+  group_by( USDA_symbol, petiole) %>% 
+  summarise( m_SLA = mean(SLA) ) 
+
+leaf_area <-
   leaf_area %>% 
   filter( plot == 'non_plot') %>% 
+  filter( total_area > 0) %>% 
   select( slice, USDA_symbol, plant_number, leaf_number, total_area, scan_date, notes) %>%
   mutate( leaf_number = ifelse(is.na(leaf_number), toupper(str_extract(slice, '(all)|(ALL)')), leaf_number)) %>% 
   mutate( petiole_type = str_extract(notes, '.*petiole')) %>% 
+  filter( petiole_type != 'with_petiole' | is.na(petiole_type) ) %>% 
+  select( -petiole_type, -notes) %>% 
   mutate( tissue_type = NA) %>% 
-  mutate( tissue_type = ifelse(str_detect(slice, 'bracts'), 'bracts', 'leaves')) %>% 
-  group_by( USDA_symbol, plant_number, petiole_type, scan_date) %>% 
-  summarise( leaf_area = sum(total_area))
+  mutate( tissue_type = ifelse(str_detect(slice, 'bracts'), 'bracts', 'leaves')) %>%
+  group_by( USDA_symbol, plant_number, leaf_number, scan_date, tissue_type) %>% 
+  mutate( id = row_number())  %>% 
+  select(-slice) %>% 
+  spread( leaf_number, total_area, fill = 0) %>% 
+  mutate( complete = ALL != 0 ) %>% 
+  mutate( total  = `1` + `2` + `3` + `ALL`) %>% 
+  group_by( USDA_symbol, plant_number, scan_date) %>% 
+  summarise( canopy_LA = sum(total), complete = all(complete) )
 
 agb <- 
   agb %>%
@@ -34,26 +51,6 @@ agb <-
   group_by(USDA_symbol, plant_number, tissue_type) %>% 
   mutate( repeats = n())
 
-canopy <- 
-  canopy %>%  
-  rename( 'alias' = species) %>% 
-  left_join(alias) %>% 
-  select(-alias)
-
-canopy %>% 
-  ggplot( aes( x = USDA_symbol, y = height )) + geom_point()
-
-canopy %>% 
-  ggplot( aes( x = USDA_symbol, y = width )) + geom_point()
-
-canopy %>% 
-  ggplot( aes( x = USDA_symbol, y = length )) + geom_point()
-
-canopy %>% 
-  mutate( area = width*height ) %>% 
-  ggplot(aes( x = USDA_symbol, y = area, label = plant_number)) + 
-  geom_label()
-
 agb <- 
   agb %>% 
   rename('mass_g' = aboveground_biomass_g) %>% 
@@ -66,6 +63,12 @@ agb <-
   spread(tissue_type, mass_g, fill = 0) %>% 
   mutate( total = leaves + stem + unclassified)
 
+canopy <- 
+  canopy %>%  
+  rename( 'alias' = species) %>% 
+  left_join(alias) %>% 
+  select(-alias)
+
 canopy_stats <- 
   canopy %>% 
   ungroup() %>% 
@@ -77,10 +80,22 @@ canopy_stats <-
 
 canopy_stats <- 
   canopy_stats %>% 
-  filter( is.na(petiole_type) | petiole_type == 'no_petiole')
+  left_join(avg_SLA %>% filter( !petiole), by = 'USDA_symbol') %>% 
+  select(USDA_symbol, plant_number, height:length, canopy_LA, complete, leaves, stem, unclassified, total, m_SLA) %>% 
+  mutate( canopy_LA_by_weight = m_SLA*leaves) %>%
+  mutate( canopy_LA  = ifelse(complete, canopy_LA, NA)) %>% 
+  mutate( canopy_LA_by_weight = ifelse( canopy_LA_by_weight == 0, NA, canopy_LA_by_weight))
 
-agb %>% filter( USDA_symbol == 'CHGL')
+canopy_stats %>% 
+  mutate( canopy_LA_by_weight = ifelse(canopy_LA_by_weight == 0, NA, canopy_LA_by_weight)) %>% 
+  ggplot( aes( x = canopy_LA, y = canopy_LA_by_weight)) + 
+  geom_point() + 
+  facet_wrap(~USDA_symbol, scales = 'free')
 
-canopy_stats %>% filter( USDA_symbol == 'CHGL')
+canopy_stats <- 
+  canopy_stats %>% 
+  mutate( projected_area = pi*(1/4*width*length) ) %>% 
+  select( USDA_symbol, plant_number, height, projected_area, canopy_LA, canopy_LA_by_weight, total) %>% 
+  rename( 'total_agb_g' = total)
 
 
