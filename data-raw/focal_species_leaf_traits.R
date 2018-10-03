10a506949d9382b3c5c2ca5480c53062bc9faffd
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-10-02 17:47:19 -0700

    Add script to write out focal species leaf traits

---
+++ data-raw/focal_species_leaf_traits.R
@@ -0,0 +1,16 @@
+rm(list = ls())
+library(tidyverse)
+library(stringr)
+
+outfile <- 'data-raw/focal_species_leaf_traits.csv'
+
+leaf_traits <- read_csv('data-raw/clean_leaf_traits.csv')
+
+focal_species_avgs <- 
+  leaf_traits %>% 
+  filter( plot != 'non_plot') %>% 
+  group_by(USDA_symbol) %>% 
+  summarise( SLA = mean(SLA, na.rm = T), LDMC = mean(LDMC, na.rm = T), LA = mean(LA, na.rm = T)) %>% 
+  mutate( type = 'focal')
+
+write_csv(focal_species_avgs, outfile)
