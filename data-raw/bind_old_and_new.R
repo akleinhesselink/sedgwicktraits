9a6e7ac5204da184766c3d0ebd70138c22fe5bea
Andy <andy.kleinhesselink@gmail.com>
2019-01-23 17:25:28 -0800

    Add gaurav's 2016 trait data for focal species
    Fills in trait data gaps for a few species including
    BRMA, VUMA, MICA and CHGL

--- data-raw/bind_old_and_new.R
+++ data-raw/bind_old_and_new.R
@@ -7,17 +7,19 @@ library(sedgwickspecies)
 outfile <- 'data/sedgwick_traits.Rdata'
 
 new <- read_csv( 'data-raw/cleaned_trait_data/clean_all_2017_traits.csv')
-old <- read_csv('data-raw/old-data/tapioca_trait_averages.csv')
+tapioca <- read_csv('data-raw/old-data/tapioca_trait_averages.csv')
+gk_2016 <- read_csv('data-raw/old-data/2016_sp_avg.csv')
 alias <- read_csv('data-raw/alias.csv')
 avg_tlp <- read_csv('data-raw/cleaned_trait_data/clean_tlp.csv')
 
 new <- 
   new %>% 
-  mutate( dataset = '2017')
+  mutate( dataset = '2017') %>% 
+  left_join(avg_tlp) %>% 
+  rename('turgor_loss_point' = tlp)
 
-
-old <- 
-  old %>% 
+tapioca <- 
+  tapioca %>% 
   mutate( `phenology` = `phenology (corrected May 2016- frame shift error)`) %>% 
   mutate( seed_mass_data_source = 'TAPIOCA', 
           notes = '', 
@@ -39,16 +41,41 @@ old <-
   left_join(avg_tlp) %>% 
   rename('turgor_loss_point' = tlp)
 
+gk_2016 <- 
+  gk_2016 %>% 
+  mutate( dataset = '2016', 
+          notes = '', 
+          seed_mass_data_source = '2016', 
+          max_height_data_source = '2016', 
+          LAR = NA, 
+          SRL = fine_root_length_m/fine_root_dry_mass_g, 
+          seed_size = NA) %>% 
+  rename( 'alias' = species, 
+          'leaf_size' = leaf_area_cm2, 
+          'SLA' = sla_cm2_g, 
+          'LDMC' = ldmc_mg_g, 
+          'LAI' = lai_la_canopy, 
+          'seed_mass' = seed_mass_g, 
+          'relative_spread' = relative_spread_lateral_height, 
+          'max_height' = height_to_infloresence_cm, 
+          'rooting_depth' = rooting_depth_cm)  %>% 
+  left_join(alias) %>%
+  left_join(avg_tlp) %>% 
+  rename('turgor_loss_point' = tlp) %>% 
+  select( alias, max_height, rooting_depth, relative_spread, LAI:LDMC, d13C:turgor_loss_point)
+
 sedgwicktraits <- 
-  bind_rows(new, old) %>% 
+  bind_rows(new, tapioca, gk_2016) %>% 
   select( -alias,   - `phenology (corrected May 2016- frame shift error)`, -`phenology (DOY 50% fruit)`)
 
 sedgwicktraits <- 
   sedgwicktraits %>% 
   left_join(sedgwick_plants, by = 'USDA_symbol') %>% 
-  select( calflora_binomial, `leaf_size`:`turgor_loss_point` ) %>% 
+  select( calflora_binomial, leaf_size:leaf_pH ) %>% 
   rename( 'species' = calflora_binomial) %>%
-  distinct()
+  distinct() %>% 
+  group_by(species) %>% 
+  mutate( turgor_loss_point = mean(turgor_loss_point, na.rm = T)) ### Correct for duplicate tlp
 
 usethis::use_data(sedgwicktraits, overwrite = T)
 
