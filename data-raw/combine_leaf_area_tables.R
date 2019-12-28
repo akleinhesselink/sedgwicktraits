dc82b247329959d15b039a4bac1a03250789e2a9
andy <andy.kleinhesselink@gmail.com>
2019-12-27 22:29:31 -0800

    Rewrite combine leaf area script
    Script now does all the formatting and combining and adds correct USDA_symbol outputs
    a csv file with individual leaf areas and total leaf area of canopy.
    
    Also rename some folders with scan tables for leaf area to match dmy date format

--- data-raw/combine_leaf_area_tables.R
+++ data-raw/combine_leaf_area_tables.R
@@ -3,10 +3,15 @@ rm(list = ls())
 library(stringr)
 library(tidyverse)
 library(sedgwickspecies)
+library(lubridate)
 
 outfile <- 'data-raw/raw_trait_data/leaf_area.csv'
 
-file_names <- dir(path = 'data-raw/all-scans', pattern = '*.xls$', recursive = 'T', full.names = T) 
+file_names <- dir(path = 'data-raw/all-scans', 
+                  pattern = '(*.xls$)|(*.csv$)', 
+                  recursive = 'T', 
+                  full.names = T) 
+
 alias <- read_csv('data-raw/alias.csv')
 
 files <- data.frame(file_name = file_names)
@@ -16,13 +21,16 @@ files <-
   mutate( notes = ifelse( str_detect('with_petiole', string = file_name), 'with_petiole', '')  ) %>% 
   mutate( notes = ifelse( str_detect('no_petiole', string = file_name), 'no_petiole', notes)) %>% 
   mutate( raw_date = str_extract(string = file_names, '[0-9]{8}')) %>% 
-  mutate( scan_date = as.Date( raw_date, format = '%d%m%Y'))
+  mutate( scan_date = dmy( raw_date))
 
 dat <- list()
 
 for ( i in 1:nrow(files)){ 
-
-  temp <- read.csv(as.character( files$file_name[i] ), sep = '\t')
+  if( str_detect(files$file_name[i], 'csv$' )){
+    temp <- read_csv(as.character(files$file_name[i]))
+  }else if( str_detect(as.character(files$file_name[i]), 'xls$')){
+    temp <- read_tsv(as.character( files$file_name[i] ))
+  }
   temp$notes <- files$notes[i]
   temp$scan_date <- files$scan_date[i]
   temp$file <- files$file_name[i]
@@ -38,47 +46,22 @@ leaf_area <-
   leaf_area %>% 
   mutate( Slice = ifelse( Slice == 'lomu-p3-l1', 'lomu-p1-all', Slice))  
 
-leaf_area <- 
-  leaf_area %>% 
-  filter( !str_detect( Slice, 'avfa-p7-l3-\\d$'))
-
-# remove slices with no area ------------------------------------------
-
-leaf_area <- 
-  leaf_area %>% 
-  filter( X.Area > 0 )
-
 # ---------------------------------------------------------------------
 
-leaf_area$Species <- str_extract( leaf_area$Slice, '^[A-Za-z]{2,}[1-9]?(?=[\\-_]{1})')
-leaf_area$plant <- tolower(str_extract( leaf_area$Slice, pattern = '[Pp][0-9][0-9]?'))
-leaf_area$leaf <- tolower(str_extract( leaf_area$Slice, pattern = '[Ll][0-9]{1}+' ))
-
 leaf_area <- 
   leaf_area %>% 
-  mutate( leaf = ifelse( is.na(leaf), tolower( str_extract(file, '(?<=l)\\d+(?=.xls)')), leaf))
+  mutate( Slice = str_to_upper(Slice)) %>% 
+  mutate( Slice = str_remove(Slice, '\\.JP[E]?G$')) %>% 
+  mutate( Slice = str_replace(Slice, 'LVS$', 'ALL'))  %>% 
+  separate( Slice, c('alias', 'plant', 'leaf'))  %>% 
+  mutate( all = str_detect( leaf, 'ALL'), petiole = str_detect(notes, 'with_petiole')) %>% 
+  mutate( plant = str_extract(plant, '\\d+'), leaf = str_extract(leaf, '\\d+')) %>% 
+  rename( 'count' = Count, 'total_area' = `Total Area`) %>% 
+  mutate( plot = 'non_plot', year = year(scan_date) ) %>% 
+  left_join( alias ) %>% 
+  select( plot, year, scan_date, USDA_symbol, all, plant, leaf, count, total_area, petiole, scan_date, file, notes)  %>% 
+  distinct() 
 
-
-leaf_area <- 
-  leaf_area %>% 
-  mutate(all = is.na(leaf)) %>% 
-  select( Slice, Species, plant, leaf, all, Count, Total.Area, scan_date, notes) %>% 
-  rename( 'slice' = Slice, 'count' = Count, 'total_area' = Total.Area, 'alias' = Species) %>% 
-  mutate( leaf = str_extract(leaf, '\\d+'), plant = str_extract(plant, '\\d+'))
-
-leaf_area <- 
-  leaf_area %>% 
-  mutate( alias = toupper(alias)) %>% 
-  left_join( alias, by = 'alias') %>% 
-  mutate( petiole = ifelse(notes == 'with_petiole', T, F))
-
-leaf_area <- 
-  leaf_area %>% 
-  rename(plant_number = plant, leaf_number= leaf) %>% 
-  mutate( scan_date = lubridate::as_date( scan_date )) %>% 
-  mutate( plant_number = str_extract(plant_number, '\\d+'), 
-          leaf_number = str_extract(leaf_number, '\\d+')) %>% 
-  mutate( plot = 'non_plot')
 
 leaf_area %>% 
   write_csv(outfile)
